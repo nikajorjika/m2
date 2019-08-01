f8d6b7dcc3792f1097c59de1d64b54302dd81a36
beqramo <beka@redberry.ge>
2019-08-01 16:26:50 +0400

    added some scss variables for future use please let team know if you change any of them in near future

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,9 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        m2Paveleon
-      </h1>
-      <h2 class="subtitle">
-        Projects server for frontend of m2 paveleon application
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
+export default {}
 </script>
 
 <style>
