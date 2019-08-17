1b391a728ccd3c0576525e37dfbc028f15a56109
bidzina <bidzinaaivazashvili@gmail.com>
2019-08-17 18:53:55 +0400

    modified components

--- components/core/SliderDesc.vue
+++ components/core/SliderDesc.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
     <div class="slider-wrapper">
-      <Slider :items="items" />
+      <Slider @messageFromSlider="messageFromSlider" :items="items" />
     </div>
     <div class="desc-wrapper">
-      <div class="desc_item" v-for="(item, index) in items" :key="item.id">
-        <Description v-if="index == 1" :title="item.title" :text="item.text" />
+      <div v-for="(item, index) in items" :key="item.id" class="desc_item">
+        <Description v-if="index == activeItem" :title="item.title" :text="item.text" />
       </div>
     </div>
   </div>
@@ -20,7 +20,23 @@ export default {
     Description,
     Slider
   },
-  props: ['items']
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      activeItem: 0
+    }
+  },
+
+  methods: {
+    messageFromSlider(count) {
+      this.activeItem = count
+    }
+  }
 }
 </script>
 
