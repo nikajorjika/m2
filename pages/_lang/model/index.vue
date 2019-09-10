a9b31942efa20f6791656591f612669773fb1c4a
kavela <kavelashvili20@gmail.com>
2019-09-10 12:59:32 +0400

    Model index page

--- pages/_lang/model/index.vue
+++ pages/_lang/model/index.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="model-page">
-
+  <div id="test" class="model-page">
+    <article>
+      <h3>
+        {{
+          model.name && model.name.hasOwnProperty(locale)
+            ? model.name[locale]
+            : ''
+        }}
+      </h3>
+    </article>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapActions, mapMutations } from 'vuex'
+
 export default {
-  layout: 'ModelIndexLayout'
+  layout: 'ModelIndexLayout',
+  computed: {
+    ...mapGetters({
+      locale: 'locale',
+      model: 'model/model'
+    })
+  },
+  mounted() {
+    this.$store.dispatch('model/fetchModel')
+  },
+  methods: {
+    ...mapActions(['model/fetchModel']),
+    ...mapMutations(['model/setModel'])
+  }
 }
 </script>
+
+<style lang="scss" scoped></style>
---
+++ store/model.js
@@ -0,0 +1,37 @@
+const state = () => ({
+  model: {}
+})
+
+const getters = {
+  model: (state) => state.model
+}
+
+const actions = {
+  fetchModel({ commit }) {
+    return new Promise((resolve, reject) => {
+      this.$axios
+        .get('/api/projects')
+        .then(({ data }) => {
+          commit('setModel', data.data[0])
+          resolve(data.data)
+        })
+        .catch((e) => reject(e))
+    })
+  }
+}
+
+const mutations = {
+  setModel(state, data) {
+    state.model = data
+  }
+}
+
+// export const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+}
