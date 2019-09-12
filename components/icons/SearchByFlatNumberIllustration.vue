8ea14387e56220c8ac572b410e7b7d9277027709
kavela <kavelashvili20@gmail.com>
2019-09-12 17:50:50 +0400

    Style: Buttons on the model index page. Add SVG icons

---
+++ components/icons/SearchByFlatNumberIllustration.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+    src="@/assets/illustrations/illustration-room.png"
+    :style="customStyles"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: [String],
+      default: '75px'
+    },
+    height: {
+      type: [String],
+      default: '72px'
+    },
+    margin: {
+      type: String,
+      default: 'initial'
+    }
+  },
+  computed: {
+    customStyles() {
+      return {
+        width: this.width,
+        height: this.height,
+        margin: this.margin
+      }
+    }
+  }
+}
+</script>
---
+++ components/icons/SearchByPhoneNumberIllustration.vue
@@ -0,0 +1,34 @@
+<template>
+  <img
+    src="@/assets/illustrations/illustration-authorisation.png"
+    :style="customStyles"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    width: {
+      type: [String],
+      default: '83px'
+    },
+    height: {
+      type: [String],
+      default: '70px'
+    },
+    margin: {
+      type: String,
+      default: 'initial'
+    }
+  },
+  computed: {
+    customStyles() {
+      return {
+        width: this.width,
+        height: this.height,
+        margin: this.margin
+      }
+    }
+  }
+}
+</script>
