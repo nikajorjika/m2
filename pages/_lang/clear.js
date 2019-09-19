46cf6fac738951278f319e1438ea03183c3e27a8
nikajorjika <n_jorjoliani@cu.edu.ge>
2019-09-19 07:41:01 +0400

    added loading screen and redirection if app is already set

--- pages/_lang/clear.js
+++ pages/_lang/clear.js
@@ -2,6 +2,7 @@ export default {
   mounted() {
     this.$cookies.remove('paveleon-app')
     this.$cookies.remove('paveleon-flat')
+    this.$cookies.remove('paveleon-planshet')
     this.$router.push({ path: `/${this.$store.getters.locale}` })
   },
   render() {
