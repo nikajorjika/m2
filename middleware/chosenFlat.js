c3139f57910d1f6bc7d4ffd8ecc240e66021d547
nikajorjika <n_jorjoliani@cu.edu.ge>
2019-09-20 15:12:51 +0400

    fix did not work

--- middleware/chosenFlat.js
+++ middleware/chosenFlat.js
@@ -1,5 +1,8 @@
 export default async function({ app, store, route, redirect }) {
-  if (route.name !== 'lang-renovations-choose') {
+  if (
+    route.name !== 'lang-renovations-choose' &&
+    !app.$cookies.get('paveleon-flat')
+  ) {
     redirect(`/${route.params.lang}/renovations/choose`)
   } else if (
     Object.entries(store.getters['Flats/flat']).length === 0 &&
