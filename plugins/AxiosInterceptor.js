99958be86f2d5f3300e7e1d3d2268aae102bf330
nikajorjika <n_jorjoliani@cu.edu.ge>
2019-09-26 20:05:04 +0400

    added two urls

--- plugins/AxiosInterceptor.js
+++ plugins/AxiosInterceptor.js
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import axios from 'axios'
+const urls = {
+  secure: process.env.SERVER_IP_SECURE,
+  notSecure: process.env.SERVER_IP
+}
+const url = this.$cookies.get('paveleon-planshet')
+  ? urls.notSecure
+  : urls.secure
+// eslint-disable-next-line no-undef
+axios.interceptors.request.use(base(url))
 
 axios.interceptors.request.use(
   (config) => {
-    // Do something before request is sent
+    config.url = url
     return config
   },
   function(error) {
-    // Do something with request error
     return Promise.reject(error)
   }
 )
