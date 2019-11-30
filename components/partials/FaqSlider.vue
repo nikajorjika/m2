7baf1bc5d38baafe71a7a15c81f23179db8655a0
imedacf <imedacf@gmail.com>
2019-11-30 15:03:06 +0400

    hardest commit ever

--- components/partials/FaqSlider.vue
+++ components/partials/FaqSlider.vue
@@ -111,7 +111,7 @@ export default {
       color: #494949;
       font-family: $font;
       font-size: 13px;
-      letter-spacing: 1px;
+      letter-spacing: .2px;
       word-spacing: 4px;
       line-height: 1.5;
       margin-top: 20px;
